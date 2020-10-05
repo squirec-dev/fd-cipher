db9c794f344d043b1c32deb33ea524403685df76
unknown <squirec.dev@gmail.com>
2020-10-05 14:06:33 +0100

    feat: default decryption

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const ciphers = {
     'pigLatin': {
-        encrypt: (text) => PigLat(text),
+        encrypt: (text) => PigLatEncrypt(text),
+        decrypt: (text) => PigLatDecrypt(text),
     },
 
 };
 
-const PigLat = (text) => text.split(' ').map(word => `${
+const PigLatEncrypt = (text) => text.split(' ').map(word => `${
         word.slice(1, word.length)
     }${
         word.charAt(0)
     }ay`).join(` `);
 
+const PigLatDecrypt = (text) => text.split(' ').map(word => `${
+        word.charAt(word.length - 3)
+    }${
+        word.slice(0, -3)
+    }`).join(` `);
+
 const encrypt = (text, method = 'pigLatin') => {
     return ciphers[method].encrypt(text);
+};
+const decrypt = (text, method = 'pigLatin') => {
+    return ciphers[method].decrypt(text);
 };
 
 module.exports = {
     encrypt,
+    decrypt,
 }
--- test/index.js
+++ test/index.js
@@ -1,10 +1,18 @@
 const { expect } = require('chai');
-const { encrypt } = require('../index');
+const { encrypt, decrypt } = require('../index');
 
 describe('Encrypt:', () => {
-    it('Given a string, returns an encrypted', () => {
+    it('Default; Given a string, returns encrypted', () => {
         expect(encrypt(
             'grisedale tarn',
         )).to.equal('risedalegay arntay');
+    });
+});
+
+describe('Decrypt:', () => {
+    it('Default; Given a string, returns decrypted', () => {
+        expect(decrypt(
+            'risedalegay arntay',
+        )).to.equal('grisedale tarn');
     });
 });
