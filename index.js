6f94ac1622b8d97664a5f02317a297ad5f3c26d5
unknown <squirec.dev@gmail.com>
2020-10-05 19:22:22 +0100

    feat: rotation cipher

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const ciphers = {
-    'pigLatin': {
-        encrypt: (text) => PigLatEncrypt(text),
-        decrypt: (text) => PigLatDecrypt(text),
-    },
+const ALPHA_REGEX = new RegExp(/[A-Za-z]/);
+const ALPHABET = [...'abcdefghijklmnopqrstuvwxyz'];
 
-};
-
-const PigLatEncrypt = (text) => text.split(' ').map(word => `${
+const EncryptPigLat = (text) => text.split(' ').map(word => `${
         word.slice(1, word.length)
     }${
         word.charAt(0)
     }ay`).join(` `);
 
-const PigLatDecrypt = (text) => text.split(' ').map(word => `${
+const DecryptPigLat = (text) => text.split(' ').map(word => `${
         word.charAt(word.length - 3)
     }${
         word.slice(0, -3)
     }`).join(` `);
 
-const encrypt = (text, method = 'pigLatin') => {
-    return ciphers[method].encrypt(text);
-};
-const decrypt = (text, method = 'pigLatin') => {
-    return ciphers[method].decrypt(text);
-};
+const EncryptRotation = (text, options) => {
+    return [...text].map((char, idx) => {
+        if (char.match(ALPHA_REGEX)) {
+            const convertCase = char === char.toUpperCase();
+            let newIndex = ALPHABET.indexOf(char.toLowerCase()) + options.val;
+
+            if (newIndex > 25) {
+                newIndex -= 26
+            }
+            if (convertCase === true) {
+                return (ALPHABET[newIndex]).toUpperCase();
+            }
+            return (ALPHABET[newIndex]);
+        }
+        return char;
+    }).join('');
+}
 
 module.exports = {
-    encrypt,
-    decrypt,
+    EncryptPigLat,
+    DecryptPigLat,
+    EncryptRotation,
 }
--- test/index.js
+++ test/index.js
@@ -1,18 +1,36 @@
 const { expect } = require('chai');
-const { encrypt, decrypt } = require('../index');
+const { EncryptPigLat, DecryptPigLat, EncryptRotation } = require('../index');
 
-describe('Encrypt:', () => {
-    it('Default; Given a string, returns encrypted', () => {
-        expect(encrypt(
+describe('Pig Latin:', () => {
+    it('Encrypt; Given a string, returns encrypted', () => {
+        expect(EncryptPigLat(
             'grisedale tarn',
         )).to.equal('risedalegay arntay');
     });
-});
-
-describe('Decrypt:', () => {
-    it('Default; Given a string, returns decrypted', () => {
-        expect(decrypt(
+    
+    it('Decrypt; Given a string, returns decrypted', () => {
+        expect(DecryptPigLat(
             'risedalegay arntay',
         )).to.equal('grisedale tarn');
+    });
+});
+
+describe('Rotation:', () => {
+    it('Encrypt; Given a string & a rotation number, returns encrypted', () => {
+        expect(EncryptRotation(
+            'x y z',
+            {
+                val: 2,
+            },
+        )).to.equal('z a b');
+    });
+
+    it('Encrypt; Given a string & a rotation number, returns encrypted', () => {
+        expect(EncryptRotation(
+            'The quick brown fox jumps over the lazy dog',
+            {
+                val: 2,
+            },
+        )).to.equal('Vjg swkem dtqyp hqz lworu qxgt vjg ncba fqi');
     });
 });
